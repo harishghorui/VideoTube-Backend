ff2ec5fc44f58f242dcc54c4e9323f4c9a381303
harishghorui <harishghorui2@gmail.com>
2024-07-26 18:57:40 +0530

    Setup (MongoDB) Done- part2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("Setup Done")
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+dotenv.config({
+    path: './env'
+})
+
+import connectDB from "./db/index.js";
+
+connectDB()
+
+/*
+
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants.js";
+
+import express from "express"
+const app = express()
+
+;( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONOGDB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("Error connecting express", error)
+            throw error;
+        })
+        app.listen((process.env.PORT), () => {
+            console.log(`app is listening on port ${process.env.PORT}`);
+        })
+    }
+    catch (error) {
+        console.log("Error connecting DB", error);
+    }
+}) ()
+
+*/
